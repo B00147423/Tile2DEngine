eb1f2d5d30c68549000c1efe30ddf6794c72d619
B00147423 <b00147423@mytudublin.ie>
2025-12-07 22:46:30 +0000

    deletign stuff
    deleted ths jsons

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 #include "window.h"
 
 int main() {
-    Window window(1280, 720, "Topher Editor");
+    Window window(1280, 720, "Tile2D");
     Editor editor(window);
     editor.run();
     return 0;
